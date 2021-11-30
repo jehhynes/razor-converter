507a90adc5a464d64a90b4571ee12d172ff69325
Jon Hynes <jonh@datarg.com>
2021-11-30 11:21:02 -0500

    TextNodeConverter can convert a <script runat="server">

--- Telerik.RazorConverter/Razor/Converters/TextNodeConverter.cs
+++ Telerik.RazorConverter/Razor/Converters/TextNodeConverter.cs
@@ -1,6 +1,8 @@
 ﻿namespace Telerik.RazorConverter.Razor.Converters
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Telerik.RazorConverter.Razor.DOM;
     using Telerik.RazorConverter.WebForms.DOM;
 
@@ -19,14 +21,22 @@
         
         public IList<IRazorNode> ConvertNode(IWebFormsNode node)
         {
-            var srcNode = node as IWebFormsTextNode;
-            var destNode = TextNodeFactory.CreateTextNode(srcNode.Text);
-            return new IRazorNode[] { destNode };
+            if (node is IWebFormsTextNode)
+            {
+                var srcNode = node as IWebFormsTextNode;
+                var destNode = TextNodeFactory.CreateTextNode(srcNode.Text);
+                return new IRazorNode[] { destNode };
+            }
+            else
+            {
+                var srcNode = node as IWebFormsServerControlNode;
+                return node.Children.OfType<IWebFormsTextNode>().Select(x => TextNodeFactory.CreateTextNode(x.Text)).ToArray();
+            }
         }
 
         public bool CanConvertNode(IWebFormsNode node)
         {
-            return node is IWebFormsTextNode;
+            return node is IWebFormsTextNode || (node is IWebFormsServerControlNode serverControlNode && serverControlNode.TagName.ToLowerInvariant() == "script" && serverControlNode.Children.All(x => x is IWebFormsTextNode));
         }
     }
 }
